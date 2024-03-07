4a75567b3f8ba15e1c16bfd367ffc2365952846f
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-07 17:21:35 +0100

    update deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,8 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = os.path.join(BASE_DIR,  'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' ,  'static')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +27,7 @@ SECRET_KEY = 'django-insecure-tr7wi_nj-kuh6!zjn%*6s70*f6qdodp#9pg*55(se!cyclocz-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh' , '127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -77,12 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
