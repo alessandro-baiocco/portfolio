446a15440eb34ad075dc7f2cfab8c88dfb402ebd
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-09 15:44:02 +0100

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -104,14 +104,6 @@ DATABASES = {
 }
 
 
-
-
-
-
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -148,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
