efe2ab1f6093440caa60a2dff64376e8c2e6f659
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-09 14:38:10 +0100

    added host render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-tr7wi_nj-kuh6!zjn%*6s70*f6qdodp#9pg*55(se!cyclocz-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app' , '127.0.0.1' , 'localhost:8000' , 'localhost']
+ALLOWED_HOSTS = ['.vercel.app' , '127.0.0.1' , 'localhost:8000' , 'localhost , .onrender.com']
 
 
 # Application definition
