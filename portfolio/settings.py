85244e90aa8329e41cbe61cefda4b1c14e329f60
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-03-09 16:07:18 +0100

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -60,9 +60,8 @@ MIDDLEWARE = [
 ]
 
 STORAGES = {
-    # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
@@ -145,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'myportfolio/static/'
 STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
